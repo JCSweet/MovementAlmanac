561e54fe0454eb302fbfb55b839598fe469aac56
JCSweet <45698957+JCSweet@users.noreply.github.com>
2024-03-20 14:53:39 -0600

    add EJS partials for component structure; add filter by equipment for API call

--- index.js
+++ index.js
@@ -8,29 +8,49 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 let result;
+let equipment;
+let filteredResult = [];
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { movement: result });
+  //   res.render("index.ejs", { movement: result });
+  res.render("index.ejs", { movement: filteredResult });
 });
+
+function filterResultsByEquipment(result, equipment) {
+  //   console.log(`Console log: filterByEquipment function has recieved request.`);
+  result.map((exercise) => {
+    if (exercise.equipment === equipment) {
+      return filteredResult.push(exercise);
+    }
+  });
+}
 
 app.post("/searchMovement", async (req, res) => {
   const apiEndpoint = "/name/";
   const apiParams = req.body.searchMovement;
+  equipment = req.body.equipment;
   const apiURL = `https://exercisedb.p.rapidapi.com/exercises${apiEndpoint}${apiParams}`;
   const options = {
     method: "GET",
     url: apiURL,
-    params: { limit: "10" },
+    params: { limit: "100" },
     headers: {
-        'X-RapidAPI-Key': '41613474f1mshba26fc3ffafdfcdp12fc43jsn96ff6c9c7661',
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      "X-RapidAPI-Key": "41613474f1mshba26fc3ffafdfcdp12fc43jsn96ff6c9c7661",
+      "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
     },
   };
-  console.log(`Console Log: Sending API request to: ${apiURL}`);
+  console.log(
+    `Console Log: Sending API request to: ${apiURL}. Search Term: "${apiParams}". Equipment: ${equipment}`
+  );
   try {
     const response = await axios.request(options);
     result = response.data;
-    // console.log(`Console Log: API Response: ${result}`);
+    if (equipment) {
+        filterResultsByEquipment(result, equipment);
+    } else {
+        filteredResult = result;
+    }
+    //   console.log(`Console log: Filtered Result: ${filteredResult}`);
     res.redirect("/");
   } catch (error) {
     console.error(`Console Log: There is an error: ${error.response}`);
