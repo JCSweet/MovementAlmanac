e479018b583c61d663c3560bce5a79514a2691fc
JCSweet <45698957+JCSweet@users.noreply.github.com>
2024-03-20 16:30:35 -0600

    update available equipment and update equipment filtering
    modify list of total available equipment to include everything I intend to allow for the app.
    combined certain equipment categories from the API (ex: if searching "barbell", include results for both "barbell" and "olympic barbell") to make search more user friendly.

--- index.js
+++ index.js
@@ -9,26 +9,52 @@ app.use(express.urlencoded({ extended: true }));
 
 let equipment;
 let filteredResult = [];
-let availableEquipment = ["assisted", "band", "barbell", "body weight", "dumbbell", "kettlebell", "olympic barbell", "resistance band", "weighted"]
+let availableEquipment = [
+  "band",
+  "barbell",
+  "body weight",
+  "dumbbell",
+  "kettlebell",
+  "olympic barbell",
+  "resistance band",
+];
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { movement: filteredResult });
 });
 
 function filterResultsByEquipment(result, equipment) {
-  result.map((exercise) => {
-    if (exercise.equipment === equipment) {
-      return filteredResult.push(exercise);
-    }
-  });
-}
-
-function filterResultsWithoutEquipment(result) {
-    result.map((exercise) => {
-            if (availableEquipment.includes(exercise.equipment)) {
+  switch (exercise.equipment) {
+    case "barbell":
+      result.map((exercise) => {
+        if (exercise.equipment === "barbell" || exercise.equipment === "olympic barbell") {
           return filteredResult.push(exercise);
         }
       });
+      break;
+    case "resistance band":
+      result.map((exercise) => {
+        if (exercise.equipment === "resistance band" || exercise.equipment === "band") {
+          return filteredResult.push(exercise);
+        }
+      });
+      break;
+    default:
+      result.map((exercise) => {
+        if (exercise.equipment === equipment) {
+          return filteredResult.push(exercise);
+        }
+      });
+      break;
+  }
+}
+
+function filterResultsWithoutEquipment(result) {
+  result.map((exercise) => {
+    if (availableEquipment.includes(exercise.equipment)) {
+      return filteredResult.push(exercise);
+    }
+  });
 }
 
 app.post("/searchMovement", async (req, res) => {
@@ -40,7 +66,7 @@ app.post("/searchMovement", async (req, res) => {
   const options = {
     method: "GET",
     url: apiURL,
-    params: { limit: "400" },
+    params: { limit: "100" },
     headers: {
       "X-RapidAPI-Key": "41613474f1mshba26fc3ffafdfcdp12fc43jsn96ff6c9c7661",
       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
@@ -53,9 +79,9 @@ app.post("/searchMovement", async (req, res) => {
     const response = await axios.request(options);
     const result = response.data;
     if (equipment) {
-        filterResultsByEquipment(result, equipment);
+      filterResultsByEquipment(result, equipment);
     } else {
-        filterResultsWithoutEquipment(result);
+      filterResultsWithoutEquipment(result);
     }
     res.redirect("/");
   } catch (error) {
